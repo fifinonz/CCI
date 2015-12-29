6ecb267fa8018cdfac109f801fa09890c406c5fe
Muthoni <muthoni.nguthi@betterworldsystems.co.ke>
2015-12-29 14:35:08 +0300

    added header and footer

--- app/application/views/inc/tmp.php
+++ app/application/views/inc/tmp.php
@@ -1,25 +1,12 @@
 <?php
 
 
-// *********************************************************************
-// Public Header//
-//if (!$this->ion_auth->logged_in())
-//{
+
     $this->load->view('inc/header');
     $this->load->view($main_content);
     $this->load->view('inc/footer');
-//}
-//		else
-//{
-//    // Accessed Only By Users Who are logged in!//
-//
-//    $this->load->view('inc/secure_header');
-//    $this->load->view($main_content);
-//    $this->load->view('inc/footer');
-//
-//
-//}
-//
+
+
 
 
 /* End of file 'tmp.php */
