ae4955c1c193b883b9ceac6f951e34c0c68adba6
Muthoni <muthoni.nguthi@betterworldsystems.co.ke>
2015-12-29 14:23:51 +0300

    added base url

--- app/application/config/config.php
+++ app/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/relayforlifekenya.org/app/';
+$config['base_url']	= 'http://localhost/childhoodcancerinitiative.org/app/';
 
 /*
 |--------------------------------------------------------------------------
